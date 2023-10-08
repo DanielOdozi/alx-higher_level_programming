a1a4b16e9ab17f659a0e8563bb98b7c523efb10b
root <root@DanielOdozi>
2023-10-08 21:19:09 +0100

    remove return from if

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,4 +6,4 @@ def max_integer(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] > new_list:
             new_list = my_list[i]
-            return new_list
+    return new_list
