833e903f72b3695c26f3f2f089695cf5d53253a7
root <root@DanielOdozi>
2023-10-23 15:08:09 +0100

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        jout = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{}".format(my_list[i]), end="")
+                jout += 1
+        print()
+        return jout
+    except ValueError:
+        print()
+        return jout
