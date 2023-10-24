dca06db909269833f15c9f15370731d5372dbb8f
root <root@DanielOdozi>
2023-10-24 16:04:20 +0100

    Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,8 @@
-#!/usr/bin/pytthon3
+#!/usr/bin/python3
 """Defines a class Square"""
 
 
 class Square:
-     """Class that defines properties of square.
+    """Class that defines properties of square.
     """
-    def ___init__(self) -> None:
-        """To defne the function square that does nothing."""
-        pass
+    pass
