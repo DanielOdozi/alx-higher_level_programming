18233fefb33811adb168f308485c8f22a23a598e
root <root@DanielOdozi>
2023-10-24 16:00:37 +0100

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/pytthon3
 class Square:
+    """An empty class square that does not but define a square."""
+
     def ___init__(self) -> None:
+        """To defne the function square that does nothing."""
         pass
