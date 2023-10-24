e2321550000710dbc536ad1b6dd5a714a39c1358
root <root@DanielOdozi>
2023-10-24 16:05:01 +0100

    First Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/pytthon3
 """Defines a class Square"""
 
 
 class Square:
-    """Class that defines properties of square.
+     """Class that defines properties of square.
     """
-    pass
+    def ___init__(self) -> None:
+        """To defne the function square that does nothing.
+        """
+        pass
