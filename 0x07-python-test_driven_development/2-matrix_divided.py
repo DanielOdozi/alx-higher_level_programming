ce33f63b0ac4d1410f49883a31a82bdac1977b6d
root <root@DanielOdozi>
2023-10-26 15:50:34 +0100

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,3 +2,40 @@
 '''A function to divide matrix'''
 
 
+def matrix_divided(matrix, div):
+    """Divids a matrix by the div input.
+
+    Args:
+        matrix (int): The matrix data to be divided.
+        div (int): The div is the data the matrix will be dvided with.
+
+    Raises:
+        TypeError: matrix must be a matrix (list of lists) of integers/floats.
+        TypeError: Each row of the matrix must have the same size
+        TypeError: div must be a number.
+        ZeroDivisionError: division by zero
+
+    Returns:
+        A new matrix.
+    """
+    new_matrix = []
+    first_row_length = len(matrix[0])
+    if not isinstance(matrix, list) or not matrix:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        
+    for row in matrix:
+        if len(row) != first_row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats.")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    for row in matrix:
+        new_row = [round(element / div, 2) for element in row]
+        new_matrix.append(new_row)
+    return new_matrix
