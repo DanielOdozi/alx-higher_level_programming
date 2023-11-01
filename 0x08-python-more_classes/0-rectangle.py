987cb29d8df69e310931c8ded4550e49d4c90c04
root <root@DanielOdozi>
2023-11-01 15:57:25 +0100

    Empty Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle."""
+
+
+class Rectangle:
+    """Class that defines properties of a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
