0af3e8f3ae9b781fa3ab257fc909c70ba7d49feb
root <root@DanielOdozi>
2023-11-06 18:15:31 +0100

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''An empty class BaseGeometry'''
+
+
+class BaseGeometry:
+    """Empty class.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    def ____init__(self) -> None:
+        pass
