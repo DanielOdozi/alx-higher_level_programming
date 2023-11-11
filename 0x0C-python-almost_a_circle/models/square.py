a98cffc08086ac12a43fe129e2a2550ef0919a21
root <root@DanielOdozi>
2023-11-12 00:42:44 +0100

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,7 +28,42 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """size retriever.
+
+        Returns:
+            int: size of square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size of square.
+
+        Args:
+            value (int): width of square.
+            value (int): height of square.
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''Prints a square'''
         return ("[Square] ({}) {}/{} - {}".
                 format(self.id, self.x, self.y, self.size,))
+
+    def update(self, *args, **kwargs):
+        '''Updates the attributes using *args and **kwargs'''
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
