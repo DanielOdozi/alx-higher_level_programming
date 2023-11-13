4017c4e286efcbede9b1f2634153a9eeb2df5188
root <root@DanielOdozi>
2023-11-13 12:29:45 +0100

    JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Class Base'''
 import json
+import csv
 
 
 class Base:
@@ -62,3 +63,57 @@ class Base:
         dummy.update(**dictionary)
 
         return(dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        '''returns a list of instances'''
+        filename = "{}.json".format(cls.__name__)
+
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+        except FileNotFoundError:
+            return []
+
+        objects_data = cls.from_json_string(json_string)
+        return [cls.create(**data) for data in objects_data]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''serializes list_objs to a CSV file'''
+        filename = "{}.csv".format(cls.__name__)
+
+        with open(filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    csv_writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    csv_writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        '''deserializes list_objs from a CSV file'''
+        filename = "{}.csv".format(cls.__name__)
+
+        try:
+            with open(filename, 'r') as csvfile:
+                csv_reader = csv.reader(csvfile)
+                objects_data = [list(map(int, row)) for row in csv_reader]
+        except FileNotFoundError:
+            return []
+
+        instances = []
+        for data in objects_data:
+            if cls.__name__ == "Rectangle":
+                instance = cls(1, 1)
+                instance.update(width=data[1], height=data[2], x=data[3], y=data[4])
+            elif cls.__name__ == "Square":
+                instance = cls(1)
+                instance.update(size=data[1], x=data[2], y=data[3])
+            else:
+                raise ValueError(f"Unsupported class: {cls.__name__}")
+
+            instances.append(instance)
+
+        return instances
